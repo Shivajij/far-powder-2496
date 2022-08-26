f1b17794e015b1a97eedc3354e13a489315c56f9
Shivaji Jadhav <103955158+Shivajij@users.noreply.github.com>
2022-08-26 20:26:16 +0530

    day4

---
+++ script/cart.js
@@ -0,0 +1,14 @@
+
+
+let sub=document.getElementById("sub-btn").addEventListener("click",Subtract)
+let add=document.getElementById("add-btn").addEventListener("click",Adition);
+let show=document.getElementById("qta-show");
+
+
+
+let ini_value=1;// local statorage 
+
+function Subtract(){
+    
+}
+
